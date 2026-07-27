35fd5f47ec04fcb9219b1bf7c2c6479851d91196
Efthymis Samartzis <samartzis47@gmail.com>
2026-07-27 15:30:39 +0300

    Add central API royter

---
+++ backend/app/api/router.py
@@ -0,0 +1,10 @@
+from fastapi import APIRouter
+
+from app.api.routes.health import router as health_router
+from app.api.routes.root import router as root_router
+
+
+api_router = APIRouter()
+
+api_router.include_router(root_router)
+api_router.include_router(health_router)
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 
-from app.api.routes.health import router as health_router
-from app.api.routes.root import router as root_router
+from app.api.router import api_router
 
 
 app = FastAPI(
@@ -14,5 +13,4 @@ app = FastAPI(
 )
 
 
-app.include_router(root_router)
-app.include_router(health_router)
+app.include_router(api_router)
